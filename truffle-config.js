648d33ea8cd479db0cfcffd23f719341d4fd8684
Diego <diego2000avelar@gmail.com>
2022-09-25 17:39:07 -0600

    chore: project base

---
+++ truffle-config.js
@@ -0,0 +1,19 @@
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.9",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  },
+};
